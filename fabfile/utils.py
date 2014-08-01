904cdec2d90ed055bc07b252edeb221d8b29873e
Vlad Seghete <vlad.seghete@gmail.com>
2014-08-01 15:59:20 -0500

    added remote_templates_root() helper function in utils

--- fabfile/utils.py
+++ fabfile/utils.py
@@ -23,6 +23,9 @@ def project_root():
 def remote_project_root():
     return "/vagrant"
 
+def remote_templates_root():
+    return os.path.join(remote_project_root(), "fabfile/templates")
+
 def get_config_parser():
     parser = ConfigParser.RawConfigParser()
     parser.read(os.path.join(project_root(), "config.ini"))
@@ -31,5 +34,3 @@ def get_config_parser():
 def set_hosts_from_config():
     parser = get_config_parser()
     env.hosts = parser.get('servers', env.provider).split(",")
-
-
